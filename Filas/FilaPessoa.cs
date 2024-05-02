75b0c7a4ba3bd08ac968111a164052a4a654e7a6
Felipe-Pestana <pestana@sitic.com.br>
2024-05-02 11:55:47 -0300

    Add project files.

---
+++ Filas/FilaPessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filas
+{
+    internal class FilaPessoa
+    {
+        Pessoa? head;
+        Pessoa? tail;
+
+        public FilaPessoa()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void Push(Pessoa aux)
+        {
+            if (IsEmpty())
+            {
+                head = tail = aux;
+            }
+            else
+            {
+                tail.setNext(aux);
+                tail = aux;
+            }
+        }
+
+        public void Pop()
+        {
+            if (!IsEmpty())
+            {
+                if (tail == head) //verifico se tenho um elemento só
+                {
+                    head = tail = null;
+                }
+
+                else
+                {
+                    this.head = head.getNext();
+                }
+            }
+        }
+
+
+        public bool IsEmpty()
+        {
+            return head == null && tail == null;
+        }
+    }
+}
---
+++ Filas/Program.cs
@@ -0,0 +1,19 @@
+﻿using Filas;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Fila!");
+
+        FilaPessoa filaPessoa = new();
+
+        filaPessoa.Push(new Pessoa("Odair"));
+        filaPessoa.Push(new Pessoa("Alexa"));
+        filaPessoa.Push(new Pessoa("Tio Paulo"));
+
+        filaPessoa.Pop();
+        filaPessoa.Pop();
+        filaPessoa.Pop();
+    }
+}
